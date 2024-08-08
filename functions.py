614c9cec5069d72291cf4892b04a191e5c88af78
Chuacx13 <chuachongxuan@gmail.com>
2024-08-09 03:49:42 +0800

    Added, documented and tested generate_analysis_results function

--- functions.py
+++ functions.py
@@ -414,5 +414,253 @@ def generate_operating_cash_flow_trend(driver, ticker, year):
     file_path = os.path.join(directory, f"{ticker}_operating_cash_flow_trend_{year}.csv")
     operating_cash_flow_trend_table.to_csv(file_path, index=True)
 
+def generate_analysis_results(driver, ticker, year, input_growth_rate):
+    """
+    This function scrapes financial data from a website using Selenium and performs an analysis to
+    calculate various financial metrics like Future Stock Price, Sticker Price before applying a Margin of Safety of 50% or 30%. 
+    Two EPS growth rates will be used, namely a user input growth rate and a historical growth rate.
+    P/E ratios used to calculate Future Stock Price are Default P/E ratio, Highest P/E ratio in the last 5-10 years, Lowest P/E ratio in the last 5-10 years and Median P/E ratio in the last 5-10 years.
+    The results are saved as CSV files.
+
+    Parameters:
+    driver: Selenium WebDriver instance used for webpage interaction.
+    ticker: Stock ticker symbol of the company being analyzed.
+    tear: The year for which the analysis is performed (used in naming output files).
+
+    Parameters:
+    driver (webdriver.Chrome): The Selenium WebDriver instance used for web automation.
+    ticker (str): The ticker symbol of the company to generate the final analysis report for.
+    year (str): The year for which the final analysis report is to be generated.
+    input_growth_rate (float): Estimated growth rate decided by the user. 
+    """
+     
+    chart_link = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "Chart"))
+    )
+    chart_link.click()
+
+    time.sleep(3)
+
+    # Current EPS
+    current_eps = float(driver.find_element(By.CSS_SELECTOR, "div.company-container div.col-sm:nth-of-type(2) ul.list-group li:nth-of-type(5) span").text)
+
+    # EPS Growth Rate (Historical)
+    eps_trend = pd.read_csv(f'Final_Results/{ticker}/{year}/{ticker}_equity_trend_{year}.csv')
+    eps_growth_rate_10year = eps_trend.iloc[0, 3]/100 + 1
+
+    # EPS Growth Rate (Analyst)
+    eps_growth_rate_analyst_input = input_growth_rate
+
+    # Default P/E for both potential EPS Growth Rate
+    pe_analyst = (eps_growth_rate_analyst_input - 1) * 200
+    pe_10year = (eps_growth_rate_10year - 1) * 200
+
+    # Max, Min and Median P/E 
+    median_pe_ratio = 0
+    max_pe_ratio = 0
+    min_pe_ratio = 0 
+    financials = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "dropdownMenuFinancials"))
+    )
+    financials.click()
+
+    time.sleep(3)
+
+    ratios = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "a.dropdown-item:nth-of-type(4)"))
+    )
+    ratios.click()
+
+    pe_ratios = driver.find_elements(By.CSS_SELECTOR, "#report-table tbody tr:nth-of-type(2) td.formatted-value")
+    pe_ratios_list = []
+
+    if len(pe_ratios) >= 10:
+        pe_ratios = pe_ratios[1:11]
+    elif len(pe_ratios) >= 5:
+        last_index = len(pe_ratios) - 1
+        pe_ratios = pe_ratios[1:last_index]
+    else:
+        return
+
+    for pe_ratio in pe_ratios:
+        pe_ratios_list.append(float(pe_ratio.text.replace(',', '')))
+
+    pe_ratios_list.sort()
+    n = len(pe_ratios_list)
+    if n % 2 != 0 and n >= 5:
+        median_index = n // 2
+        median_pe_ratio = pe_ratios_list[median_index]
+    elif n % 2 == 0 and n >= 5:
+        median_index_1 = n // 2 - 1
+        median_index_2 = n // 2
+        median_pe_ratio = (pe_ratios_list[median_index_1] + pe_ratios_list[median_index_2]) / 2
+
+    max_pe_ratio = max(pe_ratios_list)
+    min_pe_ratio = min(pe_ratios_list)
+
+    # Analyst Analysis
+    future_eps_analyst = current_eps * (eps_growth_rate_analyst_input ** 10)
+
+    ## Future Stock Price
+    fsp_default_pe_analyst = future_eps_analyst * pe_analyst
+    fsp_max_pe_analyst = future_eps_analyst * max_pe_ratio
+    fsp_median_pe_analyst = future_eps_analyst * median_pe_ratio
+    fsp_min_pe_analyst = future_eps_analyst * min_pe_ratio
+
+    ## Sticker Price - 15% Returns 
+    sp15_default_pe_analyst = fsp_default_pe_analyst * ((100/115)**10)
+    sp15_max_pe_analyst = fsp_max_pe_analyst * ((100/115)**10)
+    sp15_median_pe_analyst = fsp_median_pe_analyst * ((100/115)**10)
+    sp15_min_pe_analyst = fsp_min_pe_analyst * ((100/115)**10)
+
+    ## Sticker Price - 12% Returns 
+    sp12_default_pe_analyst = fsp_default_pe_analyst * ((100/112)**10)
+    sp12_max_pe_analyst = fsp_max_pe_analyst * ((100/112)**10)
+    sp12_median_pe_analyst = fsp_median_pe_analyst * ((100/112)**10)
+    sp12_min_pe_analyst = fsp_min_pe_analyst * ((100/112)**10)
+
+    ## Sticker Price - 10% Returns 
+    sp10_default_pe_analyst = fsp_default_pe_analyst * ((100/110)**10)
+    sp10_max_pe_analyst = fsp_max_pe_analyst * ((100/110)**10)
+    sp10_median_pe_analyst = fsp_median_pe_analyst * ((100/110)**10)
+    sp10_min_pe_analyst = fsp_min_pe_analyst * ((100/110)**10)
+
+    ## MOS (30%) of Sticker Price - 15% Returns 
+    mos3015_default_pe_analyst = sp15_default_pe_analyst * 0.7
+    mos3015_max_pe_analyst = sp15_max_pe_analyst * 0.7
+    mos3015_median_pe_analyst = sp15_median_pe_analyst * 0.7
+    mos3015_min_pe_analyst = sp15_min_pe_analyst * 0.7
+
+    ## MOS (50%) of Sticker Price - 15% Returns 
+    mos5015_default_pe_analyst = sp15_default_pe_analyst * 0.5
+    mos5015_max_pe_analyst = sp15_max_pe_analyst * 0.5
+    mos5015_median_pe_analyst = sp15_median_pe_analyst * 0.5
+    mos5015_min_pe_analyst = sp15_min_pe_analyst * 0.5
+
+    ## MOS (30%) of Sticker Price - 12% Returns 
+    mos3012_default_pe_analyst = sp12_default_pe_analyst * 0.7
+    mos3012_max_pe_analyst = sp12_max_pe_analyst * 0.7
+    mos3012_median_pe_analyst = sp12_median_pe_analyst * 0.7
+    mos3012_min_pe_analyst = sp12_min_pe_analyst * 0.7
+
+    ## MOS (50%) of Sticker Price - 12% Returns 
+    mos5012_default_pe_analyst = sp12_default_pe_analyst * 0.5
+    mos5012_max_pe_analyst = sp12_max_pe_analyst * 0.5
+    mos5012_median_pe_analyst = sp12_median_pe_analyst * 0.5
+    mos5012_min_pe_analyst = sp12_min_pe_analyst * 0.5
+
+    ## MOS (30%) of Sticker Price - 10% Returns 
+    mos3010_default_pe_analyst = sp10_default_pe_analyst * 0.7
+    mos3010_max_pe_analyst = sp10_max_pe_analyst * 0.7
+    mos3010_median_pe_analyst = sp10_median_pe_analyst * 0.7
+    mos3010_min_pe_analyst = sp10_min_pe_analyst * 0.7
+
+    ## MOS (50%) of Sticker Price - 10% Returns 
+    mos5010_default_pe_analyst = sp10_default_pe_analyst * 0.5
+    mos5010_max_pe_analyst = sp10_max_pe_analyst * 0.5
+    mos5010_median_pe_analyst = sp10_median_pe_analyst * 0.5
+    mos5010_min_pe_analyst = sp10_min_pe_analyst * 0.5
+
+    columns_analyst = [f'Default_P/E_Ratio({pe_analyst})', f'Highest_P/E_Ratio({max_pe_ratio})', f'Median_P/E_Ratio({median_pe_ratio})', f'Lowest_P/E_Ratio({min_pe_ratio})']
+    index = ['Future_Stock_Price', 'Sticker_Price_15%', 'MOS_30%(15)', 'MOS_50%(15)',  'Sticker_Price_12%', 'MOS_30%(12)', 'MOS_50%(12)', 'Sticker_Price_10%', 'MOS_30%(10)', 'MOS_50%(10)']
+
+    final_analyst = pd.DataFrame(columns=columns_analyst, index=index)
+
+    final_analyst.loc['Future_Stock_Price'] = [fsp_default_pe_analyst, fsp_max_pe_analyst, fsp_median_pe_analyst, fsp_min_pe_analyst]
+    final_analyst.loc['Sticker_Price_15%'] = [sp15_default_pe_analyst, sp15_max_pe_analyst, sp15_median_pe_analyst, sp15_min_pe_analyst]
+    final_analyst.loc['Sticker_Price_12%'] = [sp12_default_pe_analyst, sp12_max_pe_analyst, sp12_median_pe_analyst, sp12_min_pe_analyst]
+    final_analyst.loc['Sticker_Price_10%'] = [sp10_default_pe_analyst, sp10_max_pe_analyst, sp10_median_pe_analyst, sp10_min_pe_analyst]
+    final_analyst.loc['MOS_30%(15)'] = [mos3015_default_pe_analyst, mos3015_max_pe_analyst, mos3015_median_pe_analyst, mos3015_min_pe_analyst]
+    final_analyst.loc['MOS_50%(15)'] = [mos5015_default_pe_analyst, mos5015_max_pe_analyst, mos5015_median_pe_analyst, mos5015_min_pe_analyst]
+    final_analyst.loc['MOS_30%(12)'] = [mos3012_default_pe_analyst, mos3012_max_pe_analyst, mos3012_median_pe_analyst, mos3012_min_pe_analyst]
+    final_analyst.loc['MOS_50%(12)'] = [mos5012_default_pe_analyst, mos5012_max_pe_analyst, mos5012_median_pe_analyst, mos5012_min_pe_analyst]
+    final_analyst.loc['MOS_30%(10)'] = [mos3010_default_pe_analyst, mos3010_max_pe_analyst, mos3010_median_pe_analyst, mos3010_min_pe_analyst]
+    final_analyst.loc['MOS_50%(10)'] = [mos5010_default_pe_analyst, mos5010_max_pe_analyst, mos5010_median_pe_analyst, mos5010_min_pe_analyst]
+
+    directory = f"Final_Results/{ticker}/{year}"
+    file_path = os.path.join(directory, f"{ticker}_final_analyst({round(eps_growth_rate_analyst_input*100, 2)}%)_analysis_{year}.csv")
+    final_analyst.to_csv(file_path, index=True)
+
+    # Historical Analysis
+    future_eps_hist = current_eps * (eps_growth_rate_10year ** 10)
+
+    ## Future Stock Price
+    fsp_default_pe_hist = future_eps_hist * pe_10year
+    fsp_max_pe_hist = future_eps_hist * max_pe_ratio
+    fsp_median_pe_hist = future_eps_hist * median_pe_ratio
+    fsp_min_pe_hist = future_eps_hist * min_pe_ratio
+
+    ## Sticker Price - 15% Returns 
+    sp15_default_pe_hist = fsp_default_pe_hist * ((100/115)**10)
+    sp15_max_pe_hist = fsp_max_pe_hist * ((100/115)**10)
+    sp15_median_pe_hist = fsp_median_pe_hist * ((100/115)**10)
+    sp15_min_pe_hist = fsp_min_pe_hist * ((100/115)**10)
+
+    ## Sticker Price - 12% Returns 
+    sp12_default_pe_hist = fsp_default_pe_hist * ((100/112)**10)
+    sp12_max_pe_hist = fsp_max_pe_hist * ((100/112)**10)
+    sp12_median_pe_hist = fsp_median_pe_hist * ((100/112)**10)
+    sp12_min_pe_hist = fsp_min_pe_hist * ((100/112)**10)
+
+    ## Sticker Price - 10% Returns 
+    sp10_default_pe_hist = fsp_default_pe_hist * ((100/110)**10)
+    sp10_max_pe_hist = fsp_max_pe_hist * ((100/110)**10)
+    sp10_median_pe_hist = fsp_median_pe_hist * ((100/110)**10)
+    sp10_min_pe_hist = fsp_min_pe_hist * ((100/110)**10)
+
+    ## MOS (30%) of Sticker Price - 15% Returns 
+    mos3015_default_pe_hist = sp15_default_pe_hist * 0.7
+    mos3015_max_pe_hist = sp15_max_pe_hist * 0.7
+    mos3015_median_pe_hist = sp15_median_pe_hist * 0.7
+    mos3015_min_pe_hist = sp15_min_pe_hist * 0.7
+
+    ## MOS (50%) of Sticker Price - 15% Returns 
+    mos5015_default_pe_hist = sp15_default_pe_hist * 0.5
+    mos5015_max_pe_hist = sp15_max_pe_hist * 0.5
+    mos5015_median_pe_hist = sp15_median_pe_hist * 0.5
+    mos5015_min_pe_hist = sp15_min_pe_hist * 0.5
+
+    ## MOS (30%) of Sticker Price - 12% Returns 
+    mos3012_default_pe_hist = sp12_default_pe_hist * 0.7
+    mos3012_max_pe_hist = sp12_max_pe_hist * 0.7
+    mos3012_median_pe_hist = sp12_median_pe_hist * 0.7
+    mos3012_min_pe_hist = sp12_min_pe_hist * 0.7
+
+    ## MOS (50%) of Sticker Price - 12% Returns 
+    mos5012_default_pe_hist = sp12_default_pe_hist * 0.5
+    mos5012_max_pe_hist = sp12_max_pe_hist * 0.5
+    mos5012_median_pe_hist = sp12_median_pe_hist * 0.5
+    mos5012_min_pe_hist = sp12_min_pe_hist * 0.5
+
+    ## MOS (30%) of Sticker Price - 10% Returns 
+    mos3010_default_pe_hist = sp10_default_pe_hist * 0.7
+    mos3010_max_pe_hist = sp10_max_pe_hist * 0.7
+    mos3010_median_pe_hist = sp10_median_pe_hist * 0.7
+    mos3010_min_pe_hist = sp10_min_pe_hist * 0.7
+
+    ## MOS (50%) of Sticker Price - 10% Returns 
+    mos5010_default_pe_hist = sp10_default_pe_hist * 0.5
+    mos5010_max_pe_hist = sp10_max_pe_hist * 0.5
+    mos5010_median_pe_hist = sp10_median_pe_hist * 0.5
+    mos5010_min_pe_hist = sp10_min_pe_hist * 0.5
+
+    columns_hist = [f'Default_P/E_Ratio({pe_10year})', f'Highest_P/E_Ratio({max_pe_ratio})', f'Median_P/E_Ratio({median_pe_ratio})', f'Lowest_P/E_Ratio({min_pe_ratio})']
+
+    final_hist = pd.DataFrame(columns=columns_hist, index=index)
+
+    final_hist.loc['Future_Stock_Price'] = [fsp_default_pe_hist, fsp_max_pe_hist, fsp_median_pe_hist, fsp_min_pe_hist]
+    final_hist.loc['Sticker_Price_15%'] = [sp15_default_pe_hist, sp15_max_pe_hist, sp15_median_pe_hist, sp15_min_pe_hist]
+    final_hist.loc['Sticker_Price_12%'] = [sp12_default_pe_hist, sp12_max_pe_hist, sp12_median_pe_hist, sp12_min_pe_hist]
+    final_hist.loc['Sticker_Price_10%'] = [sp10_default_pe_hist, sp10_max_pe_hist, sp10_median_pe_hist, sp10_min_pe_hist]
+    final_hist.loc['MOS_30%(15)'] = [mos3015_default_pe_hist, mos3015_max_pe_hist, mos3015_median_pe_hist, mos3015_min_pe_hist]
+    final_hist.loc['MOS_50%(15)'] = [mos5015_default_pe_hist, mos5015_max_pe_hist, mos5015_median_pe_hist, mos5015_min_pe_hist]
+    final_hist.loc['MOS_30%(12)'] = [mos3012_default_pe_hist, mos3012_max_pe_hist, mos3012_median_pe_hist, mos3012_min_pe_hist]
+    final_hist.loc['MOS_50%(12)'] = [mos5012_default_pe_hist, mos5012_max_pe_hist, mos5012_median_pe_hist, mos5012_min_pe_hist]
+    final_hist.loc['MOS_30%(10)'] = [mos3010_default_pe_hist, mos3010_max_pe_hist, mos3010_median_pe_hist, mos3010_min_pe_hist]
+    final_hist.loc['MOS_50%(10)'] = [mos5010_default_pe_hist, mos5010_max_pe_hist, mos5010_median_pe_hist, mos5010_min_pe_hist]
+
+    file_path = os.path.join(directory, f"{ticker}_final_hist({round(eps_growth_rate_10year*100, 2)}%)_analysis_{year}.csv")
+    final_hist.to_csv(file_path, index=True)
+
 
 
--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
-from functions import generate_roic_table, generate_equity_trend, generate_eps_trend, generate_revenue_trend, generate_free_cash_flow_trend, generate_operating_cash_flow_trend
+from functions import generate_roic_table, generate_equity_trend, generate_eps_trend, generate_revenue_trend, generate_free_cash_flow_trend, generate_operating_cash_flow_trend, generate_analysis_results
 
 service = Service(executable_path="C:/Users/chuac/chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-march = ['BABA', 'LOGI', 'RBRK', 'GRAB']
-june = ['MSFT', 'INTU']
-september = ['AAPL', 'MU', 'V']
-december = ['META', 'INTC', 'ADBE', 'ASML', 'AMZN', 'TSM', 'TTD', 'TXN', 'TSLA', 'NVDA', 'NFLX', 'MCD', 'DPZ']
+march = {
+    'BABA': 1.1, 
+    'LOGI': 1.1, 
+    'RBRK': 1.05, 
+    'GRAB': 1.05
+}
+
+june = {
+    'MSFT': 1.2, 
+    'INTU': 1.05
+}
+
+september = {
+    'AAPL': 1.2, 
+    'MU': 1.1, 
+    'V': 1.1
+}
+
+december = {
+    'META': 1.2, 
+    'INTC': 1.2, 
+    'ADBE': 1.2, 
+    'ASML': 1.1, 
+    'AMZN': 1.2, 
+    'TSM': 1.2, 
+    'TTD': 1.1, 
+    'TXN': 1.1, 
+    'TSLA': 1.1, 
+    'NVDA': 1.2, 
+    'NFLX': 1.2, 
+    'MCD': 1.1, 
+    'DPZ': 1.05
+}
 # unsure = ['KLAC', 'LRCX', 'QCOM', 'AMAT', 'KEYS', 'SWKS', 'AKAM', 'CGNX', 'SSNC', 'TER']
 
 driver.get("https://discountingcashflows.com/")
 
-for ticker in march:
+for ticker, growth_rate in march.items():
     try:
         generate_roic_table(driver, ticker, '2024')
     except Exception as e:
@@ -44,6 +73,11 @@ for ticker in march:
         generate_operating_cash_flow_trend(driver, ticker, '2024')
     except Exception as e:
         print(f"Error generating Operating Cash Flow Trend for {ticker}: {e}")
+
+    try:
+        generate_analysis_results(driver, ticker, '2024', growth_rate)
+    except Exception as e:
+        print(f"Error generating final analysis for {ticker}: {e}")
 
     time.sleep(5)
 
